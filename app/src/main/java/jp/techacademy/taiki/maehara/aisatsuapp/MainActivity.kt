c7d1c6af4f5b6362fcca66367499d22ab265075e
ta-maehara <ta-maehara@kddi-tech.com>
2022-02-20 17:42:51 +0900

    TimePickerDialogで設定した時間に応じて、挨拶文言を変化させて表示できるように修正。
    TimePickerDialog表示用ボタンと、挨拶文言表示用TextViewを追加。

--- app/src/main/java/jp/techacademy/taiki/maehara/aisatsuapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/taiki/maehara/aisatsuapp/MainActivity.kt
@@ -1,11 +1,43 @@
 package jp.techacademy.taiki.maehara.aisatsuapp
 
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Log.d("UI_PARTS", "ボタンをタップしました")
+            showTimePickerDialog()
+        }
     }
+
+    private fun showTimePickerDialog() {
+        val timePickerDialog = TimePickerDialog(
+                this,
+                TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                    Log.d("UI_PARTS", "$hour:$minute")
+                    aisatsu(hour)
+                },
+                0, 0, true)
+        timePickerDialog.show()
+    }
+
+    private fun aisatsu(hour: Int){
+        if(hour >= 2 && hour < 10) {
+            text.setText("おはよう")
+        } else if(hour >= 10 && hour < 18) {
+            text.setText("こんにちは")
+        } else if((hour >= 18 && hour < 24) || (hour >= 0 && hour < 2)) {
+            text.setText("こんばんは")
+        } else {
+            text.setText("不正")
+        }
+    }
+
 }
